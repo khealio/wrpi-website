69963965eee465476e110955d4d8b4c86890f52d
Jonathan S <sakakuchijonathan@gmail.com>
2026-09-08 18:00:09 -0400

    Rewrite has begun, home page is finished

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,211 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<!-- Tab title -->
+	<title>WRPI Troy, 91.5 FM</title>
+	<!-- Ensures basic compatability/behavior predictability -->
+
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1.0"
+	/>
+
+	<!-- Includes styling from main stylesheet -->
+
+	<link
+		rel="stylesheet"
+		type="text/css"
+		href="/resources/styles/main.css"
+	/>
+
+	<!-- Favicon -->
+
+	<link
+		rel="icon"
+		href="/resources/img/favicon.ico"
+		type="image/x-icon"
+	/>
+
+	<!-- jQuery dependency for other scripts -->
+
+	<script
+		src="https://code.jquery.com/jquery-3.7.1.slim.js"
+		integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc="
+		crossorigin="anonymous"
+	></script>
+
+	<!-- Audio player script -->
+	<script src="/resources/scripts/webplayer.js"></script>
+	<!-- Embedded Content -->
+
+	<meta
+		content="https://wrpi.org/resources/img/jollyrocker.png"
+		property="og:image"
+	/>
+
+	<meta content="WRPI Troy" property="og:title" />
+
+	<meta
+		content="Listen in on 91.5 FM or at wrpi.org"
+		property="og:description"
+	/>
+
+	<meta content="https://wrpi.org" property="og:url" />
+</svelte:head>
+
+<div class="main-banner">
+	<div class="main-banner-content">
+		<div class="home-logo">
+			<!-- Logo -->
+
+			<a href="/">
+				<img
+					class="home-logo"
+					src="/resources/img/logo.png"
+					alt="WRPI Logo"
+				/>
+			</a>
+		</div>
+
+		<div class="dropdown">
+			<button class="dropbtn">Info</button>
+
+			<div class="dropdown-content">
+				<a href="/about/">About Us</a>
+				<a href="/contact/">Contact Info</a>
+				<a href="/join/">Join Us!</a>
+
+				<a href="https://rpi.edu" target="_blank">
+					About RPI 
+
+					<img
+						class="rpi-logo"
+						src="/resources/img/rpi-logo.png"
+						alt="RPI Logo"
+					/>
+				</a>
+			</div>
+		</div>
+
+		<div class="dropdown">
+			<button class="dropbtn">Programs</button>
+
+			<div class="dropdown-content">
+				<a href="/listen/">How To Listen</a>
+				<a href="/afterdark/">After Dark</a>
+				<a href="/wgoh/">What's Going On Here?</a>
+			</div>
+		</div>
+
+		<div class="dropdown">
+			<button class="dropbtn">Socials</button>
+
+			<div class="dropdown-content">
+				<a
+					href="https://www.instagram.com/wrpitroy/"
+					target="_blank"
+				>Instagram</a>
+
+				<a
+					href="https://www.facebook.com/WRPI91.5/"
+					target="_blank"
+				>Facebook</a>
+
+				<a href="https://x.com/wrpi" target="_blank">X (Twitter)</a>
+
+				<a
+					href="https://www.youtube.com/channel/UCSolGCqgW-XZlphqZbOxf0Q"
+					target="_blank"
+				>YouTube</a>
+
+				<a
+					href="https://open.spotify.com/user/l3t5b5q04i15d795pxl4pb9hq"
+					target="_blank"
+				>Spotify</a>
+			</div>
+		</div>
+
+		<div class="dropdown"><a class="dropbtn bannerbtn" href="/log">Audio Log</a></div>
+	</div>
+</div>
+
+<div class="alert-banner"><p>We are currently off-air on FM due to some technical difficulties with our antenna. The webstream is still up and running for the time being. We're on it!</p></div>
+{@render children()}
+
+<div id="audio-player">
+	<audio
+		id="player"
+		src="https://stream.wrpi.org/mp3-320.mp3"
+	></audio>
+
+	<input
+		id="playpause"
+		type="image"
+		src="/resources/img/play.png"
+		onclick={() => {
+			toggle_play();
+		}}
+		alt="Play/pause button"
+	/>
+
+	<label for="volume" class="ios-hidden">
+		Volume
+
+		<input
+			id="volume"
+			class="slider ios-hidden"
+			type="range"
+			min="0"
+			max="100"
+			value="100"
+		/>
+	</label>
+
+	<script>var copyLink = () => { navigator.clipboard.writeText("https://stream.wrpi.org/mp3-320.mp3"); document.getElementById("copyconfirm").classList.toggle('blink-in'); document.getElementById("streamlink").classList.toggle('shake'); delay(1000).then(() => {document.getElementById("copyconfirm").classList.toggle('blink-in');}); delay(200).then(() => {document.getElementById("streamlink").classList.toggle('shake');}); } var delay = (ms) => { return new Promise(resolve => setTimeout(resolve, ms)); }</script>
+
+	<button
+		id="streamlink"
+		onclick={copyLink()}
+		alt="copy stream link"
+	><img src="/resources/img/link.svg" /></button>
+
+	<div id="copyconfirm">Copied to clipboard</div>
+	<!-- Commented out due to spacing concerns -->
+	<!-- <p id="play_indicator">now playing...</p> -->
+</div>
+
+<!-- Page footer -->
+
+<footer class="footer">
+	<div id="footer-left">
+		WRPI Troy, 91.5 FM and streaming at WRPI.org 
+		<br />
+		1 WRPI Plaza, Troy, NY 12180-3590 
+		<br />
+		See 
+		<a href="/contact/">contact page</a>
+		for more details 
+		<br />
+
+		<a
+			href="https://publicfiles.fcc.gov/fm-profile/WRPI?id=55742"
+			target="_blank"
+		>FCC Public File</a>
+	</div>
+
+	<div id="footer-right">
+		<a
+			href="https://github.com/khealio/wrpi-website"
+			target="_blank"
+		>WRPI.org Project</a>
+
+		<br />
+		Built and maintained by WRPI members. 
+		<br />
+		Visit the project to learn more and contribute!
+	</div>
+</footer>
